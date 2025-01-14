198c5f16bd541709d4e7958d3652f95eb61dc1d5
LuisGamboaS <legs30011@gmail.com>
2025-01-14 11:17:55 -0300

    Urls and Views
    manejo de vistas u urls dinamicas  con datos estaticos

--- Blog_Project/posts/urls.py
+++ Blog_Project/posts/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('home/',views.home)
+    path('home/',views.home),
+    path("<int:id>/", views.post)
 ]
--- Blog_Project/posts/views.py
+++ Blog_Project/posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 posts=[
     {
@@ -18,8 +18,26 @@ def home(request):
     for post in posts:
         html += f'''
             <div>
-                <h1>{post['id']} - {post['title']}</h1>
+            <a href="/post/{post['id']}/">
+                <h1>{post['id']} - {post['title']}</h1></a>
                 <p>{post['content']}</p>
             </div>
 ''' 
     return HttpResponse(html)
+
+def post(request,id):
+    valid_id=False
+    for post in posts:
+        if post['id']== id:
+            post_dict= post
+            valid_id=True
+            break
+    if valid_id:
+        html  = f'''
+            <h1>{post_dict['title']}</h1>
+            <p>{post_dict['content']}</p>
+        '''
+        return HttpResponse(html)
+    else:
+        return HttpResponseNotFound("Post Not Available")
+    
